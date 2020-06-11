94a531cf7b30527bb123cbe396bb9a161eaaa62a
cloud-yd <yangdongaln@163.com>
2020-06-11 18:16:46 +0800

    增加照片的按比例线性缩放

--- opencv_photos/opencv_photos.cpp
+++ opencv_photos/opencv_photos.cpp
@@ -1,13 +1,18 @@
 ﻿#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
 	cv::VideoCapture cap;
-	cv::Mat frame;
+	cv::Mat frame, srcImage, dstImage;
+	cv::Size dsize;
+	float image_ratio;
+	static int width, height, init_data;
 	int i = 1;
+	static int photoIsReady = 0;
 	cap.open(0);
 	cv::namedWindow("camera", CV_WINDOW_AUTOSIZE);
 	
@@ -21,6 +26,14 @@ int main()
 		char Key = (char)cv::waitKey(30);
 		cap >> frame;
 		cv::imshow("camera", frame);
+		if (!init_data)
+		{
+			width = frame.cols;
+			image_ratio = (float)frame.cols / (float)frame.rows;
+			printf("image_ratio = %f \n", image_ratio);
+			init_data = 1;
+		}
+		
 		switch (Key)
 		{
 		case 'p':
@@ -28,9 +41,54 @@ int main()
 			i++;
 			cv::imwrite(filename, frame);
 			cv::imshow("photos", frame);
+			
+			photoIsReady = 1;
 			cv::waitKey(1000);
+			
+			break;
+
+		case 'c':
 			cv::destroyWindow("photos");
 			break;
+
+		case 'u':
+			if (photoIsReady)
+			{
+				srcImage = cv::imread("photo1.jpg");
+				if (srcImage.empty())
+				{
+					printf("%d : 图片打开失败 \n", __LINE__);
+					return -1;
+				}
+				width += 5;
+				height = (int)(width / image_ratio);
+				printf("Enlagre==> W = %d, H = %d \n", width, height);
+				dsize = cv::Size(width, height);
+				cv::namedWindow("Enlagre", cv::WINDOW_AUTOSIZE);
+				cv::resize(srcImage, dstImage, dsize, 0, 0, cv::INTER_LINEAR);
+				cv::imshow("Enlagre", dstImage);
+			}
+			break;
+
+		case 'd':
+			if (photoIsReady)
+			{
+				srcImage = cv::imread("photo1.jpg");
+				if (srcImage.empty())
+				{
+					printf("%d: 图片打开失败 \n",__LINE__);
+				}
+				if(width > 50)
+					width -= 5;
+				height = (int)(width / image_ratio);
+				printf("Shrink==> W = %d, H = %d \n", width, height);
+				//cv::namedWindow("Shrink", cv::WINDOW_AUTOSIZE);
+				dsize = cv::Size(width, height);
+				cv::resize(srcImage, dstImage, dsize, 0, 0, cv::INTER_LINEAR);
+				cv::imshow("Enlagre", dstImage);
+			}
+			break;
+
 		default:
 			break;
 		}
