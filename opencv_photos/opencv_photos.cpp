f009470c2da5434d7651ad8e946fc1f5e602724a
cloud-yd <yangdongaln@163.com>
2020-06-11 11:58:26 +0800

    按p实现拍照

---
+++ opencv_photos/opencv_photos.cpp
@@ -0,0 +1,40 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cv::VideoCapture cap;
+	cv::Mat frame;
+	int i = 1;
+	cap.open(0);
+	cv::namedWindow("camera", CV_WINDOW_AUTOSIZE);
+	
+	while (1)
+	{
+		if (!cap.isOpened())
+		{
+			printf("camera open failed \n");
+		}
+		string filename = cv::format("photo%d.jpg", i);
+		char Key = (char)cv::waitKey(30);
+		cap >> frame;
+		cv::imshow("camera", frame);
+		switch (Key)
+		{
+		case 'p':
+			cv::namedWindow("photos", CV_WINDOW_AUTOSIZE);
+			i++;
+			cv::imwrite(filename, frame);
+			cv::imshow("photos", frame);
+			cv::waitKey(1000);
+			cv::destroyWindow("photos");
+			break;
+		default:
+			break;
+		}
+	}
+	cv::destroyAllWindows();
+	return 0;
+}
